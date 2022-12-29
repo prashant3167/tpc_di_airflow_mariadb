0b1f45a1e214fb6ee3cd6d51f5457d4692924884
Prashant Gupta <prashantgupta@licious.com>
2022-12-29 19:08:36 +0100

    correct query

--- src/dags/queries/load_trade_historical_to_dim_trade.sql
+++ src/dags/queries/load_trade_historical_to_dim_trade.sql
@@ -1,113 +1,376 @@
-delete from {{ table }};
+create index IF NOT EXISTS dim_date_ind1 on master.dim_date(SK_DateID) using HASH;
 
-INSERT into {{ table }}
-WITH expanded_trade AS (
-    SELECT
-        t.*,
-        IF (
+create index IF NOT EXISTS dim_time_ind1 on master.dim_time(SecondDesc) using HASH;
+
+create index if not EXISTS dim_account_index on master.dim_account(AccountID);
+
+create index if not EXISTS dim_account_index2 on master.dim_account(EffectiveDate);
+
+create index if not EXISTS dim_account_index3 on master.dim_account(EndDate);
+
+create index if not EXISTS l on master.dim_security(Symbol);
+
+create index if not EXISTS m on master.dim_security(EndDate);
+
+create index if not EXISTS z on master.dim_security(EffectiveDate);
+
+create index if not EXISTS l on master.dim_broker(EffectiveDate);
+
+create index if not EXISTS m on master.dim_broker(EndDate);
+
+create index if not EXISTS z on master.dim_broker(BrokerID);
+
+create index if not EXISTS cut_dim_index on master.dim_broker(BrokerID);
+
+create index if not EXISTS DIM_CUS1_INDEX on master.dim_customer(CustomerID);
+
+create index if not EXISTS trade_type_ind on master.trade_type(TT_ID);
+
+create index if not EXISTS TH_IND on staging.trade_history_historical(TH_T_ID);
+
+create index if not EXISTS TH_IND2 on staging.trade_history_historical(TH_DTS);
+
+create index if not EXISTS watch_index on staging.watch_history_historical(W_C_ID, W_S_SYMB);
+
+create index if not EXISTS watch_index2 on staging.watch_history_historical(W_ACTION);
+
+create index if not EXISTS trade_historical_ind on staging.trade_historical(T_ID);
+
+create index if not EXISTS trade_historical_ind2 on staging.trade_historical(T_ST_ID);
+
+create index if not EXISTS trade_historical_ind3 on staging.trade_historical(T_TT_ID);
+
+create index if not EXISTS trade_historical_ind4 on staging.trade_historical(T_S_SYMB);
+
+INSERT into
+    {{ params.table }} with abc as (
+        SELECT
+            T.T_ID AS TradeID,
+            1 AS SK_BrokerID,
+            CASE
+                WHEN TH.TH_ST_ID = 'SBMT'
+                AND T.T_TT_ID IN ('TMB', 'TMS')
+                OR TH.TH_ST_ID = 'PNDG' THEN DATE_FORMAT(TH.TH_DTS, '%Y%m%d')
+                WHEN TH.TH_ST_ID IN ('CMPT', 'CNCL') THEN NULL
+            END AS SK_CreateDateID,
+            CASE
+                WHEN TH.TH_ST_ID = 'SBMT'
+                AND T.T_TT_ID IN ('TMB', 'TMS')
+                OR TH.TH_ST_ID = 'PNDG' THEN DATE_FORMAT(TH.TH_DTS, '%T')
+                WHEN TH.TH_ST_ID IN ('CMPT', 'CNCL') THEN NULL
+            END AS SK_CreateTimeID,
+            CASE
+                WHEN TH.TH_ST_ID = 'SBMT'
+                AND T.T_TT_ID IN ('TMB', 'TMS')
+                OR TH.TH_ST_ID = 'PNDG' THEN NULL
+                WHEN TH.TH_ST_ID IN ('CMPT', 'CNCL') THEN cast(DATE_FORMAT(TH.TH_DTS, '%Y%m%d') as int)
+            END AS SK_CloseDateID,
+            CASE
+                WHEN TH.TH_ST_ID = 'SBMT'
+                AND T.T_TT_ID IN ('TMB', 'TMS')
+                OR TH.TH_ST_ID = 'PNDG' THEN NULL
+                WHEN TH.TH_ST_ID IN ('CMPT', 'CNCL') THEN DATE_FORMAT(TH.TH_DTS, '%T')
+            END AS SK_CloseTimeID,
+            T.T_IS_CASH AS CashFlag,
+            Null AS SK_SecurityID,
+            NUll AS SK_CompanyID,
+            T.T_QTY AS Quantity,
+            T.T_BID_PRICE AS BidPrice,
+            NULL AS SK_CustomerID,
+            NULL AS SK_AccountID,
+            T.T_EXEC_NAME AS ExecutedBy,
+            T.T_TRADE_PRICE AS TradePrice,
+            T.T_CHRG AS Fee,
+            T.T_COMM AS Commission,
+            T.T_TAX AS Tax,
+            1 AS BatchID,
+            row_number() over(
+                partition by T.T_ID
+                order by
+                    TH.TH_DTS desc
+            ) as rn,
+            date(TH.TH_DTS) as TH_DTS,
+            T.T_ST_ID,
+            T.T_TT_ID,
+            T_S_SYMB,
+            T.T_CA_ID
+        FROM
+            staging.trade_historical T
+            INNER JOIN staging.trade_history_historical TH ON T.T_ID = TH.TH_T_ID
+    ),
+    trade_all as(
+        SELECT
+            TradeID,
+            SK_BrokerID,
+            SK_CreateDateID,
+            SK_CreateTimeID,
+            SK_CloseDateID,
+            SK_CloseTimeID,
+            CashFlag,
             (
-                th.TH_ST_ID = 'SBMT'
-                AND t.T_TT_ID IN ('TMB', 'TMS')
-            )
-            OR th.TH_ST_ID = 'PNDG',
-            th.TH_DTS,
-            NULL
-        ) AS creation_time,
-        IF (
-            th.TH_ST_ID IN ('CMPT', 'CNCL'),
-            th.TH_DTS,
-            NULL
-        ) AS closing_time,
-        th.TH_DTS AS action_time
-    FROM
-        staging.trade_historical t
-        JOIN staging.trade_history_historical th ON t.T_ID = th.TH_T_ID
-),
-recent_trade AS (
-    SELECT
-        T_ID,
-        T_ST_ID,
-        T_TT_ID,
-        T_IS_CASH,
-        T_S_SYMB,
-        T_QTY,
-        T_BID_PRICE,
-        T_CA_ID,
-        T_EXEC_NAME,
-        T_TRADE_PRICE,
-        T_CHRG,
-        T_COMM,
-        T_TAX,
-        creation_time,
-        closing_time
-    FROM
-        (
-            SELECT
-                T_ID,
-                FIRST_VALUE(T_ST_ID) OVER w AS T_ST_ID,
-                FIRST_VALUE(T_TT_ID) OVER w AS T_TT_ID,
-                FIRST_VALUE(T_IS_CASH) OVER w AS T_IS_CASH,
-                FIRST_VALUE(T_S_SYMB) OVER w AS T_S_SYMB,
-                FIRST_VALUE(T_QTY) OVER w AS T_QTY,
-                FIRST_VALUE(T_BID_PRICE) OVER w AS T_BID_PRICE,
-                FIRST_VALUE(T_CA_ID) OVER w AS T_CA_ID,
-                FIRST_VALUE(T_EXEC_NAME) OVER w AS T_EXEC_NAME,
-                FIRST_VALUE(T_TRADE_PRICE) OVER w AS T_TRADE_PRICE,
-                FIRST_VALUE(T_CHRG) OVER w AS T_CHRG,
-                FIRST_VALUE(T_COMM) OVER w AS T_COMM,
-                FIRST_VALUE(T_TAX) OVER w AS T_TAX,
-                LAST_VALUE(creation_time) OVER (
-                    w RANGE BETWEEN CURRENT ROW
-                    AND UNBOUNDED FOLLOWING
-                ) AS creation_time,
-                FIRST_VALUE(closing_time) OVER w AS closing_time,
-                ROW_NUMBER() OVER w AS time_order
-            FROM
-                expanded_trade WINDOW w AS (
-                    PARTITION BY T_ID
-                    ORDER BY
-                        action_time DESC
-                )
-        ) a
-    WHERE
-        a.time_order = 1
-)
-SELECT
-    T_ID AS TradeID,
-    acc.SK_BrokerID AS SK_BrokerID,
-    std.SK_DateID AS SK_CreateDateID,
-    stt.SK_TimeID AS SK_CreateTimeID,
-    en.SK_DateID AS SK_CloseDateID,
-    et.SK_TimeID AS SK_CloseTimeID,
-    sty.ST_NAME AS Status,
-    tt.TT_NAME AS Type,
-    T_IS_CASH AS CashFlag,
-    sec.SK_SecurityID AS SK_SecurityID,
-    sec.SK_CompanyID AS SK_CompanyID,
-    T_QTY AS Quantity,
-    T_BID_PRICE AS BidPrice,
-    acc.SK_CustomerID AS SK_CustomerID,
-    acc.SK_AccountID AS SK_AccountID,
-    T_EXEC_NAME AS ExecutedBy,
-    T_TRADE_PRICE AS TradePrice,
-    T_CHRG AS Fee,
-    T_COMM AS Commission,
-    T_TAX AS Tax,
-    1 AS BatchID
-FROM
-    recent_trade t
-    JOIN master.dim_date std ON std.DateValue = DATE(t.creation_time)
-    JOIN master.dim_time stt ON PARSE_TIME(
-        "%H:%M:%S",
-        stt.TimeValue
-    ) = TIME_TRUNC(TIME(t.creation_time), SECOND)
-    LEFT JOIN master.dim_date en ON en.DateValue = DATE(t.closing_time)
-    LEFT JOIN master.dim_time et ON PARSE_TIME("%H:%M:%S", et.TimeValue) = TIME_TRUNC(TIME(t.closing_time), SECOND)
-    JOIN master.status_type sty ON t.T_ST_ID = sty.ST_ID
-    JOIN master.trade_type tt ON t.T_TT_ID = tt.TT_ID
-    JOIN master.dim_security sec ON t.T_S_SYMB = sec.Symbol
-    AND DATE(t.creation_time) >= sec.EffectiveDate
-    AND DATE(t.creation_time) < sec.EndDate
-    JOIN master.dim_account acc ON t.T_CA_ID = acc.AccountID
-    AND DATE(t.creation_time) >= acc.EffectiveDate
-    AND DATE(t.creation_time) < acc.EndDate;
+                select
+                    SK_SecurityID
+                from
+                    master.dim_security
+                where
+                    T.T_S_SYMB = Symbol
+                    and T.TH_DTS >= EffectiveDate
+                    and T.TH_DTS < EndDate
+            ) as "SK_SecurityID",
+            (
+                select
+                    SK_CompanyID
+                from
+                    master.dim_security
+                where
+                    T.T_S_SYMB = Symbol
+                    and T.TH_DTS >= EffectiveDate
+                    and T.TH_DTS < EndDate
+            ) as "SK_CompanyID",
+            Quantity,
+            BidPrice,
+            T_CA_ID,
+            ExecutedBy,
+            TradePrice,
+            Fee,
+            Commission,
+            Tax,
+            BatchID,
+            TH_DTS,
+            T_ST_ID,
+            T_TT_ID,
+            T_S_SYMB
+        from
+            abc T
+        where
+            rn = 1
+    ),
+    trade_creation as (
+        select
+            TradeID,
+            min(concat(SK_CreateDateID, SK_CreateTimeID)) as trade_creation
+        from
+            trade_all
+        group by
+            TradeID
+    ),
+    final_merge as (
+        SELECT
+            t.TRADEID,
+            SK_BrokerID,
+            COALESCE(
+                (
+                    select
+                        COALESCE(SK_DateID, 0)
+                    from
+                        master.dim_date
+                    where
+                        SK_DateID = cast(SUBSTR(tc.trade_creation, 1, 8) as int)
+                ),
+                0
+            ) as "SK_CreateDateID",
+            COALESCE(
+                (
+                    select
+                        SK_TimeID
+                    from
+                        master.dim_time
+                    where
+                        SecondDesc = SUBSTR(tc.trade_creation, 9, 8)
+                ),
+                0
+            ) as "SK_CreateTimeID",
+            (
+                select
+                    SK_DateID
+                from
+                    master.dim_date
+                where
+                    SK_DateID = SK_CloseDateID
+            ) as "SK_CloseDateID",
+            (
+                select
+                    SK_TimeID
+                from
+                    master.dim_time
+                where
+                    SecondDesc = SK_CloseTimeID
+            ) as "SK_CloseTimeID",
+            ST.ST_NAME AS Status,
+            TT.TT_NAME AS "Type",
+            CashFlag,
+            SK_SecurityID,
+            SK_CompanyID,
+            Quantity,
+            BidPrice,
+            -- 0 as  SK_CustomerID,
+            -- 0 as SK_AccountID,
+            ExecutedBy,
+            TradePrice,
+            Fee,
+            Commission,
+            Tax,
+            BatchID,
+            TH_DTS,
+            T_CA_ID
+        from
+            trade_all t
+            left join trade_creation tc on t.TRADEID = tc.TRADEID
+            INNER JOIN master.status_type ST ON t.T_ST_ID = ST.ST_ID
+            INNER JOIN master.trade_type TT ON t.T_TT_ID = TT.TT_ID
+    )
+select
+    TRADEID,
+    A.SK_BrokerID,
+    SK_CreateDateID,
+    SK_CreateTimeID,
+    SK_CloseDateID,
+    SK_CloseTimeID,
+    T.Status,
+    Type,
+    CashFlag,
+    SK_SecurityID,
+    SK_CompanyID,
+    Quantity,
+    BidPrice,
+    SK_CustomerID,
+    SK_AccountID,
+    ExecutedBy,
+    TradePrice,
+    Fee,
+    Commission,
+    Tax,
+    T.BatchID
+from
+    final_merge T
+    inner join master.dim_account A on T.T_CA_ID = a.AccountID
+    and TH_DTS >= A.EffectiveDate
+    and TH_DTS < A.EndDate;
+
+-- with trade_init as(SELECT
+--         T.T_ID AS TradeID,
+--         1 AS SK_BrokerID,
+--         CASE
+--             WHEN TH.TH_ST_ID = 'SBMT'
+--             AND T.T_TT_ID IN ('TMB', 'TMS')
+--             OR TH.TH_ST_ID = 'PNDG' THEN DATE_FORMAT(TH.TH_DTS, '%Y%m%d')
+--             WHEN TH.TH_ST_ID IN ('CMPT', 'CNCL') THEN NULL
+--         END AS SK_CreateDateID,
+--         CASE
+--             WHEN TH.TH_ST_ID = 'SBMT'
+--             AND T.T_TT_ID IN ('TMB', 'TMS')
+--             OR TH.TH_ST_ID = 'PNDG' THEN DATE_FORMAT(TH.TH_DTS, '%T')
+--             WHEN TH.TH_ST_ID IN ('CMPT', 'CNCL') THEN NULL
+--         END AS SK_CreateTimeID,
+--         CASE
+--             WHEN TH.TH_ST_ID = 'SBMT'
+--             AND T.T_TT_ID IN ('TMB', 'TMS')
+--             OR TH.TH_ST_ID = 'PNDG' THEN NULL
+--             WHEN TH.TH_ST_ID IN ('CMPT', 'CNCL') THEN cast(DATE_FORMAT(TH.TH_DTS, '%Y%m%d') as int)
+--         END AS SK_CloseDateID,
+--         CASE
+--             WHEN TH.TH_ST_ID = 'SBMT'
+--             AND T.T_TT_ID IN ('TMB', 'TMS')
+--             OR TH.TH_ST_ID = 'PNDG' THEN NULL
+--             WHEN TH.TH_ST_ID IN ('CMPT', 'CNCL') THEN DATE_FORMAT(TH.TH_DTS, '%T')
+--         END AS SK_CloseTimeID,
+--         ST.ST_NAME AS Status,
+--         TT.TT_NAME AS "Type",
+--         T.T_IS_CASH AS CashFlag,
+--         S.SK_SecurityID AS SK_SecurityID,
+--         S.SK_CompanyID AS SK_CompanyID,
+--         T.T_QTY AS Quantity,
+--         T.T_BID_PRICE AS BidPrice,
+--         A.SK_CustomerID AS SK_CustomerID,
+--         A.SK_AccountID AS SK_AccountID,
+--         T.T_EXEC_NAME AS ExecutedBy,
+--         T.T_TRADE_PRICE AS TradePrice,
+--         T.T_CHRG AS Fee,
+--         T.T_COMM AS Commission,
+--         T.T_TAX AS Tax,
+--         1 AS BatchID,
+--         row_number() over(
+--             partition by T.T_ID
+--             order by
+--                 TH.TH_DTS desc
+--         ) as rn
+--     FROM
+--         staging.trade_historical T
+--         INNER JOIN staging.trade_history_historical TH ON T.T_ID = TH.TH_T_ID
+--         INNER JOIN master.status_type ST ON T.T_ST_ID = ST.ST_ID
+--         INNER JOIN master.trade_type TT ON T.T_TT_ID = TT.TT_ID
+--         inner join master.dim_security S on T.T_S_SYMB = S.Symbol
+--         and date(TH.TH_DTS) >= S.EffectiveDate
+--         and date(th.th_dts) < s.EndDate
+--         inner join master.dim_account A on T.T_CA_ID = a.AccountID
+--         and date(TH.TH_DTS) >= A.EffectiveDate
+--         and date(TH.TH_DTS) < A.EndDate
+-- ), trade_creation as (
+--     select
+--         TradeID,
+--         min(concat(SK_CreateDateID, SK_CreateTimeID)) as trade_creation
+--     from
+--         trade_init
+--     group by
+--         TradeID
+-- ),
+-- trade_all as(
+--     select
+--         *
+--     from
+--         trade_init
+--     where
+--         rn = 1
+-- )
+-- SELECT
+--     t.TRADEID,
+--     SK_BrokerID,
+--         COALESCE((select
+--             COALESCE(SK_DateID,0)
+--         from
+--             master.dim_date
+--         where
+--             SK_DateID = cast(SUBSTR(tc.trade_creation, 1, 8) as int)
+--     ),0) as "SK_CreateDateID",
+--     COALESCE((
+--         select
+--             SK_TimeID
+--         from
+--             master.dim_time
+--         where
+--             SecondDesc = SUBSTR(tc.trade_creation, 9, 8)
+--     ),0) as "SK_CreateTimeID",
+--     (
+--         select
+--             SK_DateID
+--         from
+--             master.dim_date
+--         where
+--             SK_DateID = SK_CloseDateID
+--     ) as "SK_CloseDateID",
+--     (
+--         select
+--             SK_TimeID
+--         from
+--             master.dim_time
+--         where
+--             SecondDesc = SK_CloseTimeID
+--     ) as "SK_CloseTimeID",
+--     Status,
+--     Type,
+--     CashFlag,
+--     SK_SecurityID,
+--     SK_CompanyID,
+--     Quantity,
+--     BidPrice,
+--     SK_CustomerID,
+--     SK_AccountID,
+--     ExecutedBy,
+--     TradePrice,
+--     Fee,
+--     Commission,
+--     Tax,
+--     BatchID
+-- from
+--     trade_all t
+--     left join trade_creation tc on t.TRADEID = tc.TRADEID;
--- src/dags/queries/reset/reset_dim_trade.sql
+++ src/dags/queries/reset/reset_dim_trade.sql
@@ -1,26 +1,24 @@
 DROP TABLE IF EXISTS master.dim_trade;
-CREATE TABLE
-    master.dim_trade
-(
-    TradeID         integer   NOT NULL,
-        SK_BrokerID     integer,
-        SK_CreateDateID integer   NOT NULL,
-        SK_CreateTimeID integer   NOT NULL,
-        SK_CloseDateID  integer,
-        SK_CloseTimeID  integer,
-        Status          text  NOT NULL,
-        Type            text  NOT NULL,
-        CashFlag        BOOLEAN NOT NULL,
-        SK_SecurityID   integer   NOT NULL,
-        SK_CompanyID    integer   NOT NULL,
-        Quantity        integer NOT NULL,
-        BidPrice        float NOT NULL,
-        SK_CustomerID   integer   NOT NULL,
-        SK_AccountID    integer   NOT NULL,
-        ExecutedBy      text  NOT NULL,
-        TradePrice      float,
-        Fee             float,
-        Commission      float,
-        Tax             float,
-        BatchID         integer   NOT NULL
-    );
+CREATE TABLE master.`dim_trade` (
+  `TradeID` int(11) NOT NULL,
+  `SK_BrokerID` bigint DEFAULT NULL,
+  `SK_CreateDateID` int(11) NOT NULL,
+  `SK_CreateTimeID` int(11) NOT NULL,
+  `SK_CloseDateID` int(11) DEFAULT NULL,
+  `SK_CloseTimeID` int(11) DEFAULT NULL,
+  `Status` text NOT NULL,
+  `Type` text NOT NULL,
+  `CashFlag` tinyint(1) NOT NULL,
+  `SK_SecurityID` text NOT NULL,
+  `SK_CompanyID` bigint(11) NOT NULL,
+  `Quantity` int(11) NOT NULL,
+  `BidPrice` float NOT NULL,
+  `SK_CustomerID` bigint(20) NOT NULL,
+  `SK_AccountID` bigint(20) NOT NULL,
+  `ExecutedBy` text NOT NULL,
+  `TradePrice` float DEFAULT NULL,
+  `Fee` float DEFAULT NULL,
+  `Commission` float DEFAULT NULL,
+  `Tax` float DEFAULT NULL,
+  `BatchID` int(11) NOT NULL
+  );
